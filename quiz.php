94487cb84f94beb2fd2126eb969ffd09f7ba8cc0
JoeBullet <joebullet.htc@gmail.com>
2017-06-02 01:56:16 +0200

    Rad na administraciji

--- quiz.php
+++ quiz.php
@@ -7,23 +7,23 @@
 	</head>
 	<body>
 		<main id="main500">
-		<nav>
-			<ul>
-				<li><a href="login.php">Administration</a></li>
-				<li><a href="access.php">Quiz</a></li>
-			</ul>
-		</nav>
-		<div class="clear"></div>
-		<aside>
-			<?php
-			//lista broja pitanja u <ul> <li> <a> da se može odabrati pitanje, te gumb završi <a>
-          ?>
-		</aside>
-		<section>
-		<?php
-			//tu idu pitanja
-          ?>
-		</section>
+            <nav>
+                <ul>
+                    <li><a href="login.php">Administration</a></li>
+                    <li><a href="access.php">Quiz</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+            <aside>
+                <?php
+                //lista broja pitanja u <ul> <li> <a> da se može odabrati pitanje, te gumb završi <a>
+              ?>
+            </aside>
+            <section>
+            <?php
+                //tu idu pitanja
+              ?>
+            </section>
 		</main>
 	</body>
 </html>
